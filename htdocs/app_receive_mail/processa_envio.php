ad6d5e75a12cf41facc40951e0728c59dbd317a0
Guihdiias <50056416+Guihdiias@users.noreply.github.com>
2019-10-17 13:27:25 -0300

    Add files via upload

---
+++ htdocs/app_receive_mail/processa_envio.php
@@ -0,0 +1,9 @@
+<?php
+
+	require "../../app_receive_mail/bibliotecas/PHPMailer/Exception.php";
+	require "../../app_receive_mail/bibliotecas/PHPMailer/OAuth.php";
+	require "../../app_receive_mail/bibliotecas/PHPMailer/PHPMailer.php";
+	require "../../app_receive_mail/bibliotecas/PHPMailer/POP3.php";
+	require "../../app_receive_mail/bibliotecas/PHPMailer/SMTP.php";
+
+	require "../../app_receive_mail/processa_envio.php";
